65d96cef408f7aed662d6beb8a1507c0b5399de1
Eric Hedberg <ericjhedberg@gmail.com>
2025-11-24 19:58:48 -0600

    remove the original version

--- src/models/perPageParam.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-/**
- * number of results per page.
- */
-export type perPageParam = number;
