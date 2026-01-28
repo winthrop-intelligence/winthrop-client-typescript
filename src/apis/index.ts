5b650bcd9f863189f8e67d719c934a0efc3f3c42
Eric Hedberg <ericjhedberg@gmail.com>
2026-01-28 11:51:28 -0600

    purge the repo

--- src/apis/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './DefaultApi';
-export * from './DossierApi';
-export * from './IntercollegiateApi';
-export * from './MlAthleticApi';
-export * from './ReportingApi';
-export * from './ScraperApi';
